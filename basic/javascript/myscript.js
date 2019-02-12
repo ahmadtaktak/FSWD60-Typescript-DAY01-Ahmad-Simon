9b300de7f38fbb2c7626cfc27ad126893e333ebf
IamMoony <s.blaha@live.at>
2019-02-12 10:28:16 +0100

    finish basic exercise 1

--- basic/javascript/myscript.js
+++ basic/javascript/myscript.js
@@ -1,3 +1,16 @@
-//Create an empty array with length 10
+//Create array with value from 1 to 10
 var array_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(array_numbers);
+//Create for loop
+function multiplicateNumbers() {
+    //Create loop for first array_numbers
+    for (var i = 0; i < array_numbers.length; i++) {
+        //Create loop for second array
+        for (var j = 0; j < array_numbers.length; j++) {
+            //Multiply each number from both arrays and output result in document
+            var result = array_numbers[i] + "x" + array_numbers[j] + "=" + array_numbers[i] * array_numbers[j] + "<br>";
+            document.write(result + "<br>");
+        }
+    }
+}
+multiplicateNumbers();
