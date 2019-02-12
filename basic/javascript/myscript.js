13f6834d9f9e090a7ade652506124235d0967361
IamMoony <s.blaha@live.at>
2019-02-12 09:43:12 +0100

    add basic exercise 1 folder strucutre and files

---
+++ basic/javascript/myscript.js
@@ -0,0 +1,3 @@
+//Create an empty array with length 10
+var array_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array_numbers);
---
+++ basic/javascript/myscript.ts
@@ -0,0 +1,4 @@
+//Create an empty array with length 10
+let array_numbers: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(array_numbers);
